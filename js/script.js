2bf32c7ffe0e072154d276d2bc1c68530be517aa
Muhammad Farouk <muhammad12fk@gmail.com>
2021-11-18 12:10:32 +0300

    Country Name Updating

--- js/script.js
+++ js/script.js
@@ -1,7 +1,10 @@
+let total = 5_000;
 let clicks = localStorage.getItem('clicks') ? localStorage.getItem('clicks') : '0000';
 function upadateLocalStorageBy(value){
+    clicks = value;
     localStorage.setItem('clicks', value);
     document.getElementById('clicks').innerHTML = value;
+    countNum.innerHTML = Number(localStorage.getItem('clicks')) + total;
 };
 upadateLocalStorageBy(clicks);
 function increment(){
